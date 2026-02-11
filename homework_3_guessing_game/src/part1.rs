29ec51db31f978560c8b1ccb249a7a9601d7c0e9
vaanya-jain <vaanya@bu.edu>
2026-02-11 17:07:35 -0500

    solution for part1

--- homework_3_guessing_game/src/part1.rs
+++ homework_3_guessing_game/src/part1.rs
@@ -6,7 +6,11 @@ pub struct Part1 {}
 // Terrible strategy: ask if the number is min, otherwise return max.
 impl Strategy for Part1 {
     fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
-        // YOUR SOLUTION GOES HERE.
-        todo!("No solution provided yet")
+        for i in min..max {
+            if player.ask_if_equal(i) {
+                return i;
+            }
+        }
+        min
     }
 }
