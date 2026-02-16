ae5796e42390159ebbdf59cd4985f4f244745c81
pakhi <pakhiag@bu.edu>
2026-02-16 11:07:34 -0500

    solution for part2

--- homework_3_guessing_game/src/part2.rs
+++ homework_3_guessing_game/src/part2.rs
@@ -7,6 +7,19 @@ pub struct Part2 {}
 impl Strategy for Part2 {
     fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
         // YOUR SOLUTION GOES HERE.
-        todo!("No solution provided yet")
+        let mut low = min;
+        let mut high = max;
+        while low < high {
+            let mid = low + (high - low) / 2;
+
+            match player.ask_to_compare(mid) {
+                0 => return mid,
+                -1 => high = mid,
+                1 => low = mid + 1,
+                _ => unreachable!("ask_to_compare should only return -1, 0, or 1"),
+            }
+        }
+
+        low
     }
 }
