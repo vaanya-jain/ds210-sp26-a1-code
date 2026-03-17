f9b7bf694a863ef928aa058f0a9b30d08ffa57c6
vaanya-jain <vaanya@bu.edu>
2026-03-16 21:38:26 -0400

    Defining struct

--- project_2_chatbot/basic_chatbot/src/solution/v3.rs
+++ project_2_chatbot/basic_chatbot/src/solution/v3.rs
@@ -1,4 +1,5 @@
 use kalosm::language::*;
+use std::collections::HashMap;
 
 #[allow(dead_code)]
 pub struct ChatbotV3 {
@@ -8,6 +9,8 @@ pub struct ChatbotV3 {
     // together!
     // Need to store one chat session per user.
     // Think of some kind of data structure that can help you with this.
+    model: Llama,
+    sessions: HashMap<String, Chat<Llama>>,
 }
 
 impl ChatbotV3 {
@@ -15,6 +18,8 @@ impl ChatbotV3 {
     pub fn new(model: Llama) -> ChatbotV3 {
         return ChatbotV3 {
             // Make sure you initialize your struct members here
+            model,
+            sessions: HashMap::new(),
         };
     }
 
