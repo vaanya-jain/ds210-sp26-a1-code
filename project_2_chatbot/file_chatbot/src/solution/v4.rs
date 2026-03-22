66f77422c4758016f05b83b0f483583deef8d209
pakhi <pakhiag@bu.edu>
2026-03-22 15:25:43 -0400

    implemented get_history

--- project_2_chatbot/file_chatbot/src/solution/v4.rs
+++ project_2_chatbot/file_chatbot/src/solution/v4.rs
@@ -1,5 +1,5 @@
-use kalosm::language::*;
 use crate::solution::file_library;
+use kalosm::language::*;
 
 pub struct ChatbotV4 {
     model: Llama,
@@ -7,15 +7,14 @@ pub struct ChatbotV4 {
 
 impl ChatbotV4 {
     pub fn new(model: Llama) -> ChatbotV4 {
-        return ChatbotV4 {
-            model: model,
-        };
+        return ChatbotV4 { model: model };
     }
 
     pub async fn chat_with_user(&mut self, username: String, message: String) -> String {
         let filename = &format!("{}.txt", username);
 
-        let mut chat_session: Chat<Llama> = self.model
+        let mut chat_session: Chat<Llama> = self
+            .model
             .chat()
             .with_system_prompt("The assistant will act like a pirate");
 
@@ -33,10 +32,17 @@ impl ChatbotV4 {
         match file_library::load_chat_session_from_file(&filename) {
             None => {
                 return Vec::new();
-            },
+            }
             Some(session) => {
-                // TODO: what should happen here?
-                return Vec::new();
+                let history = session.history();
+
+                let mut messages: Vec<String> = Vec::new();
+
+                for msg in history {
+                    messages.push(msg.content.clone());
+                }
+
+                return messages;
             }
         }
     }
