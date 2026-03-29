052adc4a7963f4bffb844c60a4104e8f2352825a
vaanya-jain <vaanya@bu.edu>
2026-03-29 16:54:57 -0400

    Implement filter_dataset

--- project_3_client_server_analytics/analytics_lib/src/solution.rs
+++ project_3_client_server_analytics/analytics_lib/src/solution.rs
@@ -2,8 +2,40 @@ use std::collections::HashMap;
 use crate::dataset::{ColumnType, Dataset, Value, Row};
 use crate::query::{Aggregation, Condition, Query};
 
+fn match_condition(row: &Row, dataset: &Dataset, condition: &Condition) -> bool {
+    match condition {
+        Condition::Equal(column_name, expected_value ) => {
+            let column_index = dataset.column_index(column_name);
+            let actual_value = row.get_value(column_index);
+            actual_value == expected_value
+        }
+
+        Condition::Not(inner_condition) => {
+            !match_condition(row, dataset, inner_condition)
+        }
+
+        Condition::And(left_condition, right_condition ) => {
+            match_condition(row, dataset, left_condition)
+                && match_condition(row, dataset, right_condition)
+        }
+
+        Condition::Or(left_condition, right_condition) => {
+            match_condition(row, dataset, left_condition) 
+                || match_condition(row, dataset, right_condition)
+        }
+    }
+}
+
 pub fn filter_dataset(dataset: &Dataset, filter: &Condition) -> Dataset {
-    todo!("Implement this!");
+    let mut filtered_dataset = Dataset::new(dataset.columns().clone());
+
+    for row in dataset.iter() {
+        if match_condition(row, dataset, filter) {
+            filtered_dataset.add_row(row.clone());
+        }
+    }
+
+    filtered_dataset
 }
 
 pub fn group_by_dataset(dataset: Dataset, group_by_column: &String) -> HashMap<Value, Dataset> {
