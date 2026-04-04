2db12f0fc141b1ed2ff2bf1c5a2733ec5146808a
vaanya-jain <vaanya@bu.edu>
2026-04-04 14:19:55 -0400

    Implement step1

--- project_3_client_server_analytics/analytics_lib/src/solution.rs
+++ project_3_client_server_analytics/analytics_lib/src/solution.rs
@@ -2,17 +2,110 @@ use std::collections::HashMap;
 use crate::dataset::{ColumnType, Dataset, Value, Row};
 use crate::query::{Aggregation, Condition, Query};
 
+fn match_condition(row: &Row, dataset: &Dataset, condition: &Condition) -> bool {
+    match condition {
+        Condition::Equal(column_name, expected_value ) => {
+            let column_index = dataset.column_index(column_name);
+            let actual_value = row.get_value(column_index);
+            actual_value == expected_value
+        }
+
+        Condition::Not(inner_condition) => {
+            !match_condition(row, dataset, inner_condition)
+        }
+
+        Condition::And(left_condition, right_condition ) => {
+            match_condition(row, dataset, left_condition)
+                && match_condition(row, dataset, right_condition)
+        }
+
+        Condition::Or(left_condition, right_condition) => {
+            match_condition(row, dataset, left_condition) 
+                || match_condition(row, dataset, right_condition)
+        }
+    }
+}
+
 pub fn filter_dataset(dataset: &Dataset, filter: &Condition) -> Dataset {
-    todo!("Implement this!");
+    let mut filtered_dataset = Dataset::new(dataset.columns().clone());
+
+    for row in dataset.iter() {
+        if match_condition(row, dataset, filter) {
+            filtered_dataset.add_row(row.clone());
+        }
+    }
+
+    filtered_dataset
 }
 
 pub fn group_by_dataset(dataset: Dataset, group_by_column: &String) -> HashMap<Value, Dataset> {
-    todo!("Implement this!");
+    let mut grouped = HashMap::new();
+
+    let columns = dataset.columns().clone();
+    let group_index = dataset.column_index(group_by_column);
+
+    for row in dataset.into_iter() {
+        let group_value = row.get_value(group_index).clone();
+
+        if !grouped.contains_key(&group_value) {
+            grouped.insert(group_value.clone(), Dataset::new(columns.clone()));
+        }
+
+        grouped.get_mut(&group_value).unwrap().add_row(row);
+    }
+
+    return grouped;
 }
 
 pub fn aggregate_dataset(dataset: HashMap<Value, Dataset>, aggregation: &Aggregation) -> HashMap<Value, Value> {
-    todo!("Implement this!");
+    let mut result = HashMap::new();
+    for (group_value, group_dataset) in dataset {
+        let aggregated = match aggregation {
+            Aggregation::Count(_) => {
+                Value::Integer(group_dataset.len() as i32)
+            }
+
+            Aggregation::Sum(column_name) => {
+                let idx = group_dataset.column_index(column_name);
+                let mut sum = 0;
+
+                for row in group_dataset.iter() {
+                    if let Value::Integer(num) = row.get_value(idx) {
+                        sum += *num;
+                    }
+                }
+
+                Value::Integer(sum)
+            }
+
+            Aggregation::Average(column_name) => {
+                let idx = group_dataset.column_index(column_name);
+                let mut sum = 0;
+                let mut count = 0;
+
+                for row in group_dataset.iter() {
+                    if let Value::Integer(num) = row.get_value(idx) {
+                        sum += *num;
+                        count += 1;
+                    }
+                }
+
+                if count == 0 {
+                    Value::Integer(0)
+                } else {
+                    Value::Integer(sum / count)
+                }
+            }
+        };
+
+        result.insert(group_value, aggregated);
+    }
+
+    result
+
+
 }
+
 
 pub fn compute_query_on_dataset(dataset: &Dataset, query: &Query) -> Dataset {
     let filtered = filter_dataset(dataset, query.get_filter());
