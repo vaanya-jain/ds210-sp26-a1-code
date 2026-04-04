6f852ed6c0e3baf4a1b57384a3106a7ad0972a38
Aarna Bhammar <abhammar@bu.edu>
2026-04-04 14:31:47 -0400

    server code fast rpc implementation

--- project_3_client_server_analytics/server/src/solution.rs
+++ project_3_client_server_analytics/server/src/solution.rs
@@ -1,4 +1,5 @@
 use analytics_lib::{dataset::Dataset, query::Query};
+use analytics_lib::solution::compute_query_on_dataset;
 
 pub fn hello() -> String {
     println!("hello called");
@@ -12,5 +13,5 @@ pub fn slow_rpc(input_dataset: &Dataset) -> Dataset {
 
 pub fn fast_rpc(input_dataset: &Dataset, query: Query) -> Dataset {
     println!("fast_rpc called");
-    todo!("Implement this");
+    return compute_query_on_dataset(input_dataset,&query);
 }
