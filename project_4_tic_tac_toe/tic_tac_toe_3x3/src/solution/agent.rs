50da7b0d5fc5dd04470c76d35fb513fdcd177a64
vaanya-jain <vaanya@bu.edu>
2026-04-11 12:35:12 -0400

    Implement Step1 of 3x3

--- project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
@@ -1,3 +1,5 @@
+use std::i32;
+
 use tic_tac_toe_stencil::agents::Agent;
 use tic_tac_toe_stencil::board::Board;
 use tic_tac_toe_stencil::player::Player;
@@ -13,6 +15,17 @@ impl Agent for SolutionAgent {
     fn solve(board: &mut Board, player: Player, _time_limit: u64) -> (i32, usize, usize) {
         // If you want to make a recursive call to this solution, use
         // `SolutionAgent::solve(...)`
-        unimplemented!("Not yet implemented")
+        if board.game_over() {
+            return(board.score(), 0, 0);
+        }
+
+        let moves = board.moves();
+        let mut best_score = match player {
+            Player::X => i32::MIN,
+            Player::O => i32::MAX,
+        };
+
+        let mut best_move = (0, 0);
     }
+    
 }
