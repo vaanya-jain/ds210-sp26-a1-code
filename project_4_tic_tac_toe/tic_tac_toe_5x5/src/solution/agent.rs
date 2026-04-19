0020d5cd54f93038ea7d3fef47b95dce10299a58
pakhi <pakhiag@bu.edu>
2026-04-19 16:41:15 -0400

    ran the tests

--- project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
@@ -11,6 +11,47 @@ impl Agent for SolutionAgent {
     // where <score> is your estimate for the score of the game
     // and <x>, <y> are the position of the move your solution will make.
     fn solve(board: &mut Board, player: Player, _time_limit: u64) -> (i32, usize, usize) {
-        unimplemented!("Not yet implemented")
+        if board.game_over() {
+            return (board.score(), 0, 0);
+        }
+
+        let moves = board.moves();
+
+        let mut best_score = match player {
+            Player::X => i32::MIN,
+            Player::O => i32::MAX,
+        };
+
+        let mut best_move = (0, 0);
+
+        let next_player = match player {
+            Player::X => Player::O,
+            Player::O => Player::X,
+        };
+
+        for mv in moves {
+            board.apply_move(mv, player);
+
+            let (score, _, _) = SolutionAgent::solve(board, next_player, _time_limit);
+
+            board.undo_move(mv, player);
+
+            match player {
+                Player::X => {
+                    if score > best_score {
+                        best_score = score;
+                        best_move = mv;
+                    }
+                }
+                Player::O => {
+                    if score < best_score {
+                        best_score = score;
+                        best_move = mv;
+                    }
+                }
+            }
+        }
+
+        (best_score, best_move.0, best_move.1)
     }
 }
